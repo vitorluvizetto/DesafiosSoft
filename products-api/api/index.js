e1db0c145a5b170c80142b9e494a01ded1f85d32
Vitor <vitor.dluvizetto@gmail.com>
2021-03-24 14:09:37 -0300

    esqueleto da api

---
+++ products-api/api/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+
+app.listen(3000, () => {
+    console.log('A API está funcionando!')
+});
